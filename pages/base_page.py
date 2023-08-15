cd6dc61a3ab86e24415c6b8022d1543e49dbf99b
lambotik <lambotik1988@gmail.com>
2023-08-15 19:05:37 +0300

    Demonstration automation testing UI with allure report

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,5 @@
 import allure
+from allure_commons.types import AttachmentType
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait as wait
 
@@ -7,6 +8,7 @@ class BasePage:
     def __init__(self, driver, url):
         self.driver = driver
         self.url = url
+        self.time_out = 5
 
     def open(self):
         with allure.step(f'Open page: {self.url}'):
@@ -16,9 +18,19 @@ class BasePage:
         get_url = self.url
         return get_url
 
-    def element_is_visible(self, locator, timeout=5):
+    def element_is_visible(self, locator):
         with allure.step(f'Check element is visible {locator}'):
-            return wait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
+            return wait(self.driver, self.time_out).until(EC.visibility_of_element_located(locator))
 
-    def element_is_present(self, locator, timeout=5):
-        return wait(self.driver, timeout).until(EC.presence_of_element_located(locator))
+    @allure.step('Check element is present')
+    def element_is_present(self, locator):
+        return wait(self.driver, self.time_out).until(EC.presence_of_element_located(locator))
+
+    @allure.step('Attach screenshot')
+    def attach_screenshot(self, element):
+        """Create screenshot of current window and attach it in allure report
+        Args:
+         - file_name: str like 'Linkedin_button_not_found'
+        """
+        element_name = ''.join(element)
+        allure.attach(self.driver.get_screenshot_as_png(), name=element_name, attachment_type=AttachmentType.PNG)
--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,3 +1,5 @@
+import pytest
+
 from pages.main_page import MainPage
 import allure
 
@@ -5,6 +7,7 @@ import allure
 @allure.suite('Test Main Page')
 class TestMainPage:
     @allure.step('Test fill form')
+    @pytest.mark.xfail
     def test_fill_form(self, driver):
         test_elements = MainPage(driver, 'https://allcedo.ru/')
         test_elements.open()
